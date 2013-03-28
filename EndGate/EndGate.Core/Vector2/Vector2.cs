d0bd84ae8a2ba76f100f4510c080f501e0cbcff2
N. Taylor Mullen <ntaylormullen@outlook.com>
2013-03-28 09:06:26 -0700

    Built out a Vector2 class and started creating its unit tests

---
+++ EndGate/EndGate.Core/Vector2/Vector2.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EndGate.Core
+{
+    public partial class Vector2
+    {
+        public Vector2(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public double X { get; set; }
+        public double Y { get; set; }
+
+        public void Apply(Func<double, double> action)
+        {
+            X = action(X);
+            Y = action(Y);
+        }
+
+        public bool Equivalent(Vector2 v1)
+        {
+            return v1.X == X && v1.Y == Y;
+        }
+
+        public Vector2 Add(Number val)
+        {
+            return this + val;
+        }
+
+        public Vector2 Add(Vector2 v1)
+        {
+            return this + v1;
+        }
+
+        public Vector2 Subtract(Number val)
+        {
+            return this - val;
+        }
+
+        public Vector2 SubtractFrom(Number val)
+        {
+            return val - this;
+        }
+
+        public Vector2 Subtract(Vector2 v1)
+        {
+            return this - v1;
+        }
+
+        public Vector2 Multiply(Vector2 v1)
+        {
+            return this * v1;
+        }
+
+        public Vector2 Multiply(Number val)
+        {
+            return this * val;
+        }
+
+        public Vector2 Divide(Number val)
+        {
+            return this / val;
+        }
+
+        public Vector2 DivideFrom(Number val)
+        {
+            return val / this;
+        }
+
+        public Vector2 Divide(Vector2 v1)
+        {
+            return this / v1;
+        }
+
+        public static Vector2 operator +(Vector2 v1, Vector2 v2)
+        {
+            return new Vector2(v1.X + v2.X, v1.Y + v2.Y);
+        }
+
+        public static Vector2 operator +(Vector2 v1, Number val)
+        {
+            return new Vector2(v1.X + val, v1.Y + val);
+        }
+
+        public static Vector2 operator +(Number val, Vector2 v1)
+        {
+            return v1 + val;
+        }
+
+        public static Vector2 operator -(Vector2 v1, Vector2 v2)
+        {
+            return new Vector2(v1.X - v2.X, v1.Y - v2.Y);
+        }
+
+        public static Vector2 operator -(Vector2 v1, Number val)
+        {
+            return new Vector2(v1.X - val, v1.Y - val);
+        }
+
+        public static Vector2 operator -(Number val, Vector2 v1)
+        {
+            return new Vector2(val - v1.X, val - v1.Y);
+        }
+
+        public static Vector2 operator *(Vector2 v1, Vector2 v2)
+        {
+            return new Vector2(v1.X * v2.X, v1.Y * v2.Y);
+        }
+
+        public static Vector2 operator *(Vector2 v1, Number val)
+        {
+            return new Vector2(v1.X * val, v1.Y * val);
+        }
+
+        public static Vector2 operator *(Number val, Vector2 v1)
+        {
+            return v1 * val;
+        }
+
+        public static Vector2 operator /(Vector2 v1, Vector2 v2)
+        {
+            return new Vector2(v1.X / v2.X, v1.Y / v2.Y);
+        }
+
+        public static Vector2 operator /(Vector2 v1, Number val)
+        {
+            return new Vector2(v1.X / val, v1.Y / val);
+        }
+
+        public static Vector2 operator /(Number val, Vector2 v1)
+        {
+            return new Vector2(val / v1.X, val / v1.Y);
+        }
+
+        public static Vector2 operator -(Vector2 v1)
+        {
+            return v1 * -1;
+        }
+
+        public static Vector2 Zero = new Vector2(0, 0);
+    }
+}
---
+++ EndGate/tests/EndGate.Core.Tests/Vector2Facts.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace EndGate.Core.Tests
+{
+    public class Vector2Facts
+    {
+        [Fact]
+        public void Vector2AddsCorrectly()
+        {
+            var v = new Vector2(2, 3);
+
+            Assert.False(v == v + 0);
+            Assert.False(v == v.Add(0));
+            Assert.True(v.Equivalent(v+0));
+            Assert.True(v.Equivalent(v.Add(0)));
+            Assert.True((v + v).Equivalent(new Vector2(4, 6)));
+            Assert.True(v.Add(v).Equivalent(new Vector2(4, 6)));
+            Assert.True((v + .1).Equivalent(new Vector2(2.1, 3.1)));
+            Assert.True(v.Add(.1).Equivalent(new Vector2(2.1, 3.1)));
+        }
+
+        [Fact]
+        public void Vector2SubtractsCorrectly()
+        {
+            var v = new Vector2(2, 3);
+
+            Assert.False(v == v - 0);
+            Assert.False(v == v.Subtract(0));
+            Assert.False(-v == v.SubtractFrom(0));
+            Assert.True(v.Equivalent(v - 0));
+            Assert.True(v.Equivalent(v.Subtract(0)));
+            Assert.True((-v).Equivalent(0- v));
+            Assert.True((-v).Equivalent(v.SubtractFrom(0)));
+            Assert.True((v - v).Equivalent(Vector2.Zero));
+            Assert.True((v.Subtract(v)).Equivalent(Vector2.Zero));
+            Assert.True((v - .1).Equivalent(new Vector2(1.9, 2.9)));
+            Assert.True((v.Subtract(.1)).Equivalent(new Vector2(1.9, 2.9)));
+            Assert.True((.1 - v).Equivalent(new Vector2(-1.9, -2.9)));
+            Assert.True((v.SubtractFrom(.1)).Equivalent(new Vector2(-1.9, -2.9)));
+        }
+
+        [Fact]
+        public void Vector2MultipliesCorrectly()
+        {
+            var v = new Vector2(2, 3);
+
+            Assert.False(Vector2.Zero == v * 0);
+            Assert.False(Vector2.Zero == v.Multiply(0));
+            Assert.True(Vector2.Zero.Equivalent(v * 0));
+            Assert.True(Vector2.Zero.Equivalent(v.Multiply(0)));
+            Assert.True((v * v).Equivalent(new Vector2(4, 9)));
+            Assert.True(v.Multiply(v).Equivalent(new Vector2(4, 9)));
+            Assert.True((v * .5).Equivalent(new Vector2(1, 1.5)));
+            Assert.True(v.Multiply(.5).Equivalent(new Vector2(1, 1.5)));
+        }
+
+        [Fact]
+        public void Vector2DividesCorrectly()
+        {
+            var v = new Vector2(2, 4);
+
+            Assert.False(v == v / 1);
+            Assert.False(v == v.Divide(1));
+            Assert.True(v.Equivalent(v / 1));
+            Assert.True(v.Equivalent(v.Divide(1)));
+            Assert.True(new Vector2(.5, .25).Equivalent(1 / v));
+            Assert.True(new Vector2(.5, .25).Equivalent(v.DivideFrom(1)));
+            Assert.True(new Vector2(20, 40).Equivalent(v / .1));
+            Assert.True(new Vector2(20, 40).Equivalent(v.Divide(.1)));
+        }
+    }
+}
