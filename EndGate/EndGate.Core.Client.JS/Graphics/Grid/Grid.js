bf101f8457f644774d78c60b02057b995ea64d1c
N. Taylor Mullen <ntaylormullen@outlook.com>
2013-05-04 01:30:29 -0700

    Added a Grid class to handle tile based row mechanics

---
+++ EndGate/EndGate.Core.Client.JS/Graphics/Grid/Grid.js
@@ -0,0 +1,124 @@
+var __extends = this.__extends || function (d, b) {
+    function __() { this.constructor = d; }
+    __.prototype = b.prototype;
+    d.prototype = new __();
+};
+var EndGate;
+(function (EndGate) {
+    (function (Graphics) {
+        var Grid = (function (_super) {
+            __extends(Grid, _super);
+            function Grid(x, y, rows, columns, tileWidth, tileHeight, drawGridLines, color) {
+                if (typeof drawGridLines === "undefined") { drawGridLines = false; }
+                if (typeof color === "undefined") { color = "gray"; }
+                        _super.call(this, new EndGate.Vector2d(x, y));
+                this._type = "Grid";
+                var halfSize, topLeft, bottomRight;
+                this._size = new EndGate.Size2d(tileWidth * columns, tileHeight * rows);
+                this._tileSize = new EndGate.Size2d(tileWidth, tileHeight);
+                this._grid = [];
+                this._rows = rows;
+                this._columns = columns;
+                this._drawGridLines = drawGridLines;
+                this._gridLines = [];
+                halfSize = this._size.Multiply(.5);
+                topLeft = new EndGate.Vector2d(-halfSize.Width, -halfSize.Height);
+                bottomRight = new EndGate.Vector2d(halfSize.Width, halfSize.Height);
+                for(var i = 0; i < rows; i++) {
+                    this._grid[i] = [];
+                    this._gridLines.push(new Graphics.Line2d(topLeft.X, topLeft.Y + i * this._tileSize.Height, bottomRight.X, topLeft.Y + i * this._tileSize.Height, 1));
+                    for(var j = 0; j < columns; j++) {
+                        if(i === 0) {
+                            this._gridLines.push(new Graphics.Line2d(topLeft.X + j * this._tileSize.Width, topLeft.Y, topLeft.X + j * this._tileSize.Width, bottomRight.Y, 1));
+                        }
+                        this._grid[i].push(null);
+                    }
+                }
+                this._gridLines.push(new Graphics.Line2d(topLeft.X, bottomRight.Y, bottomRight.X, bottomRight.Y, 1));
+                this._gridLines.push(new Graphics.Line2d(bottomRight.X, topLeft.Y, bottomRight.X, bottomRight.Y, 1));
+                this.Color(color);
+            }
+            Grid.prototype.Color = function (color) {
+                if(typeof color !== "undefined") {
+                    this._gridLineColor = color;
+                    for(var i = 0; i < this._gridLines.length; i++) {
+                        this._gridLines[i].Color(color);
+                    }
+                }
+                return this._gridLineColor;
+            };
+            Grid.prototype.Size = function () {
+                return this._size.Clone();
+            };
+            Grid.prototype.TileSize = function () {
+                return this._tileSize.Clone();
+            };
+            Grid.prototype.Opacity = function (alpha) {
+                return this.State.GlobalAlpha(alpha);
+            };
+            Grid.prototype.Fill = function (row, column, graphic) {
+                row--;
+                column--;
+                graphic.Position = this.GetInsideGridPosition(row, column);
+                this._grid[row][column] = graphic;
+                this.AddChild(graphic);
+            };
+            Grid.prototype.FillSpace = function (row, column, graphicList) {
+                var graphic;
+                row--;
+                column--;
+                for(var i = 0; i < graphicList.length; i++) {
+                    for(var j = 0; j < graphicList[i].length; j++) {
+                        graphic = graphicList[i][j];
+                        graphic.Position = this.GetInsideGridPosition(i + row, j + column);
+                        this._grid[i + row][j + column] = graphic;
+                        this.AddChild(graphic);
+                    }
+                }
+            };
+            Grid.prototype.FillRow = function (row, graphicList, offset) {
+                if (typeof offset === "undefined") { offset = 0; }
+                var graphic;
+                row--;
+                for(var i = 0; i < graphicList.length; i++) {
+                    graphic = graphicList[i];
+                    graphic.Position = this.GetInsideGridPosition(row, i + offset);
+                    this._grid[row][i + offset] = graphic;
+                    this.AddChild(graphic);
+                }
+            };
+            Grid.prototype.FillColumn = function (column, graphicList, offset) {
+                if (typeof offset === "undefined") { offset = 0; }
+                var graphic;
+                column--;
+                for(var i = 0; i < graphicList.length; i++) {
+                    graphic = graphicList[i];
+                    graphic.Position = this.GetInsideGridPosition(i + offset, column);
+                    this._grid[i + offset][column] = graphic;
+                    this.AddChild(graphic);
+                }
+            };
+            Grid.prototype.Draw = function (context) {
+                _super.prototype.StartDraw.call(this, context);
+                if(this._drawGridLines) {
+                    for(var i = 0; i < this._gridLines.length; i++) {
+                        this._gridLines[i].Draw(context);
+                    }
+                }
+                _super.prototype.EndDraw.call(this, context);
+            };
+            Grid.prototype.GetDrawBounds = function () {
+                var bounds = new EndGate.Bounds.BoundingRectangle(this.Position, this._size);
+                bounds.Rotation = this.Rotation;
+                return bounds;
+            };
+            Grid.prototype.GetInsideGridPosition = function (row, column) {
+                return new EndGate.Vector2d(column * this._tileSize.Width - this._size.HalfWidth() + this._tileSize.HalfWidth(), row * this._tileSize.Height - this._size.HalfHeight() + this._tileSize.HalfHeight());
+            };
+            return Grid;
+        })(Graphics.Abstractions.Graphic2d);
+        Graphics.Grid = Grid;        
+    })(EndGate.Graphics || (EndGate.Graphics = {}));
+    var Graphics = EndGate.Graphics;
+})(EndGate || (EndGate = {}));
+//@ sourceMappingURL=Grid.js.map
---
+++ EndGate/EndGate.Core.Client.JS/Graphics/Grid/Grid.ts
@@ -0,0 +1,162 @@
+/// <reference path="../../Assets/Vectors/Vector2d.ts" />
+/// <reference path="../../Assets/Sizes/Size2d.ts" />
+/// <reference path="../Graphic2d.ts" />
+/// <reference path="../Line2d.ts" />
+
+module EndGate.Graphics {
+
+    export class Grid extends Abstractions.Graphic2d {
+        public _type: string = "Grid";
+
+        private _size: Size2d;
+        private _tileSize: Size2d;
+        private _grid: Abstractions.Graphic2d[][];
+        private _gridLines: Line2d[];
+        private _positionOffset: Vector2d;
+        private _rows: number;
+        private _columns: number;
+        private _drawGridLines: bool;
+        private _gridLineColor: string;
+
+        constructor(x: number, y: number, rows: number, columns: number, tileWidth: number, tileHeight: number, drawGridLines?: bool = false, color?: string = "gray") {
+            super(new Vector2d(x, y));
+            var halfSize: Size2d,
+                topLeft: Vector2d,
+                bottomRight: Vector2d;
+
+            this._size = new Size2d(tileWidth * columns, tileHeight * rows);
+            this._tileSize = new Size2d(tileWidth, tileHeight);
+            this._grid = [];
+            this._rows = rows;
+            this._columns = columns;
+            this._drawGridLines = drawGridLines;
+            this._gridLines = [];
+
+            halfSize = this._size.Multiply(.5);
+            topLeft = new Vector2d(-halfSize.Width, -halfSize.Height);
+            bottomRight = new Vector2d(halfSize.Width, halfSize.Height);
+
+            for (var i = 0; i < rows; i++) {
+                this._grid[i] = [];
+                this._gridLines.push(new Line2d(topLeft.X, topLeft.Y + i * this._tileSize.Height, bottomRight.X, topLeft.Y + i * this._tileSize.Height, 1));
+
+                for (var j = 0; j < columns; j++) {
+                    if (i === 0) {
+                        this._gridLines.push(new Line2d(topLeft.X + j * this._tileSize.Width, topLeft.Y, topLeft.X + j * this._tileSize.Width, bottomRight.Y, 1));
+                    }
+
+                    this._grid[i].push(null);
+                }
+            }
+
+            this._gridLines.push(new Line2d(topLeft.X, bottomRight.Y, bottomRight.X, bottomRight.Y, 1));
+            this._gridLines.push(new Line2d(bottomRight.X, topLeft.Y, bottomRight.X, bottomRight.Y, 1));
+
+            this.Color(color);
+        }
+
+        public Color(color?: string): string {
+            if (typeof color !== "undefined") {
+                this._gridLineColor = color;
+
+                for (var i = 0; i < this._gridLines.length; i++) {
+                    this._gridLines[i].Color(color);
+                }
+            }
+
+            return this._gridLineColor;
+        }
+
+        public Size(): Size2d {
+            return this._size.Clone();
+        }
+
+        public TileSize(): Size2d {
+            return this._tileSize.Clone();
+        }
+
+        public Opacity(alpha?: number): number {
+            return this.State.GlobalAlpha(alpha);
+        }
+
+        public Fill(row: number, column: number, graphic: Abstractions.Graphic2d): void {
+            row--;
+            column--;
+            graphic.Position = this.GetInsideGridPosition(row, column);
+
+            this._grid[row][column] = graphic;
+            this.AddChild(graphic);
+        }
+
+        public FillSpace(row: number, column: number, graphicList: Abstractions.Graphic2d[][]): void {
+            var graphic: Abstractions.Graphic2d;
+
+            row--;
+            column--;
+
+            for (var i = 0; i < graphicList.length; i++) {
+                for (var j = 0; j < graphicList[i].length; j++) {
+                    graphic = graphicList[i][j];
+                    graphic.Position = this.GetInsideGridPosition(i + row, j + column);
+
+                    this._grid[i + row][j + column] = graphic;
+                    this.AddChild(graphic);
+                }
+            }
+        }
+
+        public FillRow(row: number, graphicList: Abstractions.Graphic2d[], offset?: number = 0): void {
+            var graphic: Abstractions.Graphic2d;
+
+            row--;
+
+            for (var i = 0; i < graphicList.length; i++) {
+                graphic = graphicList[i];
+                graphic.Position = this.GetInsideGridPosition(row, i + offset);
+
+                this._grid[row][i + offset] = graphic;
+                this.AddChild(graphic);
+            }
+        }
+
+        public FillColumn(column: number, graphicList: Abstractions.Graphic2d[], offset?: number = 0): void {
+            var graphic: Abstractions.Graphic2d;
+
+            column--;
+
+            for (var i = 0; i < graphicList.length; i++) {
+                graphic = graphicList[i];
+                graphic.Position = this.GetInsideGridPosition(i + offset, column);
+
+                this._grid[i + offset][column] = graphic;
+                this.AddChild(graphic);
+            }
+        }
+
+        public Draw(context: CanvasRenderingContext2D): void {
+            super.StartDraw(context);
+
+            // No need to draw the grid  items because the base Graphic2d class will handle all of the children drawing for me
+            if (this._drawGridLines) {
+                for (var i = 0; i < this._gridLines.length; i++) {
+                    this._gridLines[i].Draw(context);
+                }
+            }
+
+            super.EndDraw(context);
+        }
+
+        public GetDrawBounds(): Bounds.Abstractions.Bounds2d {
+            var bounds = new Bounds.BoundingRectangle(this.Position, this._size);
+
+            bounds.Rotation = this.Rotation;
+
+            return bounds;
+        }
+
+        private GetInsideGridPosition(row: number, column: number): Vector2d {
+            return new Vector2d(column * this._tileSize.Width - this._size.HalfWidth() + this._tileSize.HalfWidth(), row * this._tileSize.Height - this._size.HalfHeight() + this._tileSize.HalfHeight());
+        }
+    }
+
+}
