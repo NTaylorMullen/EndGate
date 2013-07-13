f99d680a8763126698c1902a6d593bc085626d67
N. Taylor Mullen <ntaylormullen@outlook.com>
2013-07-13 00:31:40 -0700

    Added tests to verify correctness of Graphic2d children Visible property being used correctly

---
+++ EndGate/tests/EndGate.Core.JS.Tests/Tests/Graphics/Graphic2dFacts.js
@@ -0,0 +1,43 @@
+﻿/// <reference path="../../Scripts/endgate.js" />
+
+(function () {
+
+    QUnit.module("Graphic2d Facts");
+
+    QUnit.test("Graphic2d's have children's draw triggered.", function () {
+        var parent = new eg.Graphics.Circle(0,0,3),
+            child = new eg.Graphics.Circle(0,0,3),
+            dummyCanvas = document.createElement("canvas"),
+            drawCalled = false;
+
+        parent.AddChild(child);
+
+        child.Draw = function () {
+            drawCalled = true;
+        };
+
+        parent.Draw(dummyCanvas.getContext("2d"));
+
+        QUnit.ok(drawCalled);
+    });
+
+    QUnit.test("Graphic2d's children are not called if not visible.", function () {
+        var parent = new eg.Graphics.Circle(0, 0, 3),
+            child = new eg.Graphics.Circle(0, 0, 3),
+            dummyCanvas = document.createElement("canvas"),
+            drawCalled = false;
+
+        parent.AddChild(child);
+
+        child.Visible = false;
+
+        child.Draw = function () {
+            drawCalled = true;
+        };
+
+        parent.Draw(dummyCanvas.getContext("2d"));
+
+        QUnit.ok(!drawCalled);
+    });
+
+})();
