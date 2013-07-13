e8dccafb53d7bd53d97181ceeb7b44b1f6136405
N. Taylor Mullen <ntaylormullen@outlook.com>
2013-07-13 00:51:19 -0700

    Added tests to verify the correctness of how many times OnChange is triggered.

---
+++ EndGate/tests/EndGate.Core.JS.Tests/Tests/Tweening/TweenFacts.js
@@ -0,0 +1,30 @@
+﻿/// <reference path="../../Scripts/endgate.js" />
+
+(function () {
+
+    QUnit.module("Tween Facts");
+
+    QUnit.test("Tween's have onChange triggered on the last tween interval.", function () {
+        var from = 0,
+            to = 3,
+            gameTime = new eg.GameTime(),
+            tween = new eg.Tweening.NumberTween(from, to, eg.TimeSpan.FromSeconds(.3), eg.Tweening.Functions.Linear.EaseNone);
+
+        tween.Play();
+
+        tween.OnChange.Bind(function (newVal) {
+            from += newVal;
+        });
+
+        // 1 + 2 + 3 = 6
+
+        gameTime.Elapsed.Milliseconds = 100;
+        tween.Update(gameTime);
+        QUnit.equal(from, 1);
+        tween.Update(gameTime);
+        QUnit.equal(from, 3);
+        tween.Update(gameTime);
+        QUnit.equal(from, 6);
+    });
+
+})();
