746c3feb5a007c57c5b6f28480ee326b7fd0ad46
N. Taylor Mullen <ntaylormullen@outlook.com>
2013-07-27 20:39:19 -0700

    Made the SquareTileMap load its data asynchronously.  Also added bindings to tie into the async loading.
    #36
    #35

--- EndGate/EndGate.Core.JS/Map/TileMaps/SquareTileMap.js
+++ EndGate/EndGate.Core.JS/Map/TileMaps/SquareTileMap.js
@@ -7,8 +7,12 @@ var __extends = this.__extends || function (d, b) {
 var EndGate;
 (function (EndGate) {
     /// <reference path="../../Assets/Sizes/Size2d.ts" />
+    /// <reference path="../../Assets/TimeSpan.ts" />
     /// <reference path="../../Graphics/Sprites/ImageSource.ts" />
     /// <reference path="../../Graphics/Grid/Grid.ts" />
+    /// <reference path="../../Utilities/EventHandler2.ts" />
+    /// <reference path="../../Utilities/EventHandler.ts" />
+    /// <reference path="../../Extensions/Helpers.ts" />
     /// <reference path="TileMap.ts" />
     /// <reference path="SquareTile.ts" />
     (function (Map) {
@@ -20,17 +24,53 @@ var EndGate;
             function SquareTileMap(x, y, tileWidth, tileHeight, resources, mappings, staticMap, drawGridLines) {
                 if (typeof staticMap === "undefined") { staticMap = true; }
                 if (typeof drawGridLines === "undefined") { drawGridLines = false; }
+                var _this = this;
                 _super.call(this, x, y, resources);
 
                 this._grid = new EndGate.Graphics.Grid(0, 0, mappings.length, mappings[0].length, tileWidth, tileHeight, drawGridLines);
                 this._staticMap = staticMap;
-
-                this.FillGridWith(mappings);
+                this._onTileLoad = new EndGate.EventHandler2();
+                this._onLoaded = new EndGate.EventHandler();
+                this._loaded = false;
+                this._tilesBuilt = 0;
+                this._totalTiles = this._grid.Rows * this._grid.Columns;
+                this.TileLoadDelay = EndGate.TimeSpan.Zero;
+                this.RowLoadDelay = EndGate.TimeSpan.Zero;
 
                 if (this._staticMap) {
                     this.BuildCache();
                 }
+
+                // Execute this on the next stack, to allow time for binding to the tile maps load events
+                setTimeout(function () {
+                    _this.FillGridWith(mappings, function () {
+                        _this._loaded = true;
+                        _this._onLoaded.Trigger();
+                    });
+                }, 0);
             }
+            Object.defineProperty(SquareTileMap.prototype, "OnTileLoad", {
+                get: /**
+                * Gets an event that is triggered when a tile has been loaded, first argument is the tile that was loaded, second is the percent complete.  Once this SquareTileMap has been created and all tiles loaded this event will no longer be triggered. Functions can be bound or unbound to this event to be executed when the event triggers.
+                */
+                function () {
+                    return this._onTileLoad;
+                },
+                enumerable: true,
+                configurable: true
+            });
+
+            Object.defineProperty(SquareTileMap.prototype, "OnLoaded", {
+                get: /**
+                * Gets an event that is triggered when the square tile map has been loaded.  Once this SquareTileMap has been created and all tiles loaded this event will no longer be triggered. Functions can be bound or unbound to this event to be executed when the event triggers.
+                */
+                function () {
+                    return this._onLoaded;
+                },
+                enumerable: true,
+                configurable: true
+            });
+
             SquareTileMap.ExtractTiles = /**
             * Helper function used to take a SpriteSheet image and create a one dimensional resource tile array.
             * @param imageSource The sprite sheet to extract the tile resources from.
@@ -47,6 +87,13 @@ var EndGate;
                 }
 
                 return resources;
+            };
+
+            /**
+            * Determines if the current SquareTileMap is loaded.
+            */
+            SquareTileMap.prototype.IsLoaded = function () {
+                return this._loaded;
             };
 
             /**
@@ -82,28 +129,75 @@ var EndGate;
                 this._mapCache = document.createElement("canvas");
                 this._mapCache.width = size.Width;
                 this._mapCache.height = size.Height;
-
-                // Draw the grid onto the cached map
-                this._grid.Position = new EndGate.Vector2d(size.HalfWidth, size.HalfHeight);
-                this._grid.Draw(this._mapCache.getContext("2d"));
-                this._grid.Position = originalPosition;
+                this._mapCacheContext = this._mapCache.getContext("2d");
+                this._mapCacheContext.translate(size.HalfWidth, size.HalfHeight);
             };
 
-            SquareTileMap.prototype.FillGridWith = function (mappings) {
-                var tiles = [];
+            SquareTileMap.prototype.CacheTile = function (tile) {
+                // Draw the tile onto the map cache
+                tile.Draw(this._mapCacheContext);
+            };
 
-                for (var i = 0; i < mappings.length; i++) {
-                    tiles[i] = [];
-                    for (var j = 0; j < mappings[i].length; j++) {
-                        if (mappings[i][j] >= 0) {
-                            tiles[i].push(new Map.SquareTile(this._Resources[mappings[i][j]], this._grid.TileSize.Width, this._grid.TileSize.Height));
-                        } else {
-                            tiles[i].push(null);
-                        }
+            SquareTileMap.prototype.FillGridWith = function (mappings, onComplete) {
+                var _this = this;
+                asyncLoop(function (next, rowsComplete) {
+                    _this.AsyncBuildGridRow(rowsComplete, mappings, function () {
+                        next();
+                    });
+                }, mappings.length, function () {
+                    onComplete();
+                });
+            };
+
+            SquareTileMap.prototype.AsyncBuildGridTile = function (row, column, tileGraphic, onComplete) {
+                var _this = this;
+                var action = function () {
+                    var tile;
+
+                    tile = new Map.SquareTile(tileGraphic, _this._grid.TileSize.Width, _this._grid.TileSize.Height);
+
+                    _this._grid.Fill(row, column, tile);
+
+                    if (_this._staticMap) {
+                        _this.CacheTile(tile);
                     }
-                }
 
-                this._grid.FillSpace(0, 0, tiles);
+                    _this.OnTileLoad.Trigger(tile, _this._tilesBuilt / _this._totalTiles);
+
+                    onComplete(tile);
+                };
+
+                if (this.TileLoadDelay.Milliseconds > 0) {
+                    setTimeout(action, this.TileLoadDelay.Milliseconds);
+                } else {
+                    action();
+                }
+            };
+
+            // Only pretend async in order to free up the DOM
+            SquareTileMap.prototype.AsyncBuildGridRow = function (rowIndex, mappings, onComplete) {
+                var _this = this;
+                var action = function () {
+                    asyncLoop(function (next, tilesLoaded) {
+                        _this._tilesBuilt++;
+
+                        if (mappings[rowIndex][tilesLoaded] >= 0) {
+                            _this.AsyncBuildGridTile(rowIndex, tilesLoaded, _this._Resources[mappings[rowIndex][tilesLoaded]], function (tile) {
+                                next();
+                            });
+                        } else {
+                            next();
+                        }
+                    }, mappings[rowIndex].length, function () {
+                        onComplete();
+                    });
+                };
+
+                if (this.RowLoadDelay.Milliseconds > 0) {
+                    setTimeout(action, this.RowLoadDelay.Milliseconds);
+                } else {
+                    action();
+                }
             };
             return SquareTileMap;
         })(Map.TileMap);
--- EndGate/EndGate.Core.JS/Map/TileMaps/SquareTileMap.ts
+++ EndGate/EndGate.Core.JS/Map/TileMaps/SquareTileMap.ts
@@ -1,6 +1,10 @@
 /// <reference path="../../Assets/Sizes/Size2d.ts" />
+/// <reference path="../../Assets/TimeSpan.ts" />
 /// <reference path="../../Graphics/Sprites/ImageSource.ts" />
 /// <reference path="../../Graphics/Grid/Grid.ts" />
+/// <reference path="../../Utilities/EventHandler2.ts" />
+/// <reference path="../../Utilities/EventHandler.ts" />
+/// <reference path="../../Extensions/Helpers.ts" />
 /// <reference path="TileMap.ts" />
 /// <reference path="SquareTile.ts" />
 
@@ -10,9 +14,24 @@ module EndGate.Map {
     * Defines a structure that is proficient at creating diverse tile maps based off of a resource image.  Best drawn via a SceneryHandler.
     */
     export class SquareTileMap extends TileMap {
+        /**
+        * Gets or sets the tile load delay component.  This can be used to slowly load a square tile map to prevent the browser from freezing by adding a delay between tile loads to allow time for the DOM to update.  Defaults to TimeSpan.Zero.
+        */
+        public TileLoadDelay: TimeSpan;
+        /**
+        * Gets or sets the row load delay component.  This can be used to slowly load a square tile map to prevent the browser from freezing by adding a delay between row loads to allow time for the DOM to update.  Defaults to TimeSpan.Zero.
+        */
+        public RowLoadDelay: TimeSpan;
+
         private _grid: Graphics.Grid;
         private _staticMap: boolean;
         private _mapCache: HTMLCanvasElement;
+        private _mapCacheContext: CanvasRenderingContext2D;
+        private _onTileLoad: EventHandler2<SquareTile, number>;
+        private _onLoaded: EventHandler;
+        private _loaded: boolean;
+        private _tilesBuilt: number;
+        private _totalTiles: number;
 
         /**
         * Creates a new instance of the SquareTileMap object.
@@ -50,14 +69,41 @@ module EndGate.Map {
         constructor(x: number, y: number, tileWidth: number, tileHeight: number, resources: Graphics.Assets.ImageSource[], mappings: number[][], staticMap: boolean = true, drawGridLines: boolean = false) {
             super(x, y, resources);
 
-            this._grid = new Graphics.Grid(0, 0, mappings.length, mappings[0].length, tileWidth, tileHeight,drawGridLines);
-            this._staticMap = staticMap;            
-
-            this.FillGridWith(mappings);
+            this._grid = new Graphics.Grid(0, 0, mappings.length, mappings[0].length, tileWidth, tileHeight, drawGridLines);
+            this._staticMap = staticMap;
+            this._onTileLoad = new EventHandler2<SquareTile, number>();
+            this._onLoaded = new EventHandler();
+            this._loaded = false;
+            this._tilesBuilt = 0;
+            this._totalTiles = this._grid.Rows * this._grid.Columns;
+            this.TileLoadDelay = TimeSpan.Zero;
+            this.RowLoadDelay = TimeSpan.Zero;
 
             if (this._staticMap) {
                 this.BuildCache();
             }
+
+            // Execute this on the next stack, to allow time for binding to the tile maps load events
+            setTimeout(() => {
+                this.FillGridWith(mappings, () => {
+                    this._loaded = true;
+                    this._onLoaded.Trigger();
+                });
+            }, 0);
+        }
+
+        /**
+        * Gets an event that is triggered when a tile has been loaded, first argument is the tile that was loaded, second is the percent complete.  Once this SquareTileMap has been created and all tiles loaded this event will no longer be triggered. Functions can be bound or unbound to this event to be executed when the event triggers.
+        */
+        public get OnTileLoad(): EventHandler2<SquareTile, number> {
+            return this._onTileLoad;
+        }
+
+        /**
+        * Gets an event that is triggered when the square tile map has been loaded.  Once this SquareTileMap has been created and all tiles loaded this event will no longer be triggered. Functions can be bound or unbound to this event to be executed when the event triggers.
+        */
+        public get OnLoaded(): EventHandler {
+            return this._onLoaded;
         }
 
         /**
@@ -66,10 +112,10 @@ module EndGate.Map {
         * @param tileWidth The width of the sprite sheet tiles.
         * @param tileHeight The height of the sprite sheet tiles.
         */
-        public static ExtractTiles(imageSource: Graphics.Assets.ImageSource, tileWidth: number, tileHeight: number): Graphics.Assets.ImageSource[]{
+        public static ExtractTiles(imageSource: Graphics.Assets.ImageSource, tileWidth: number, tileHeight: number): Graphics.Assets.ImageSource[] {
             var resources: Graphics.Assets.ImageSource[] = [],
                 framesPerRow: number = Math.floor(imageSource.ClipSize.Width / tileWidth),
-                    rows: number = Math.floor(imageSource.ClipSize.Height / tileHeight);
+                rows: number = Math.floor(imageSource.ClipSize.Height / tileHeight);
 
             for (var i = 0; i < rows; i++) {
                 for (var j = 0; j < framesPerRow; j++) {
@@ -78,6 +124,13 @@ module EndGate.Map {
             }
 
             return resources;
+        }
+
+        /**
+        * Determines if the current SquareTileMap is loaded.
+        */
+        public IsLoaded(): boolean {
+            return this._loaded;
         }
 
         /**
@@ -115,29 +168,75 @@ module EndGate.Map {
             this._mapCache = <HTMLCanvasElement>document.createElement("canvas");
             this._mapCache.width = size.Width;
             this._mapCache.height = size.Height;
-
-            // Draw the grid onto the cached map
-            this._grid.Position = new Vector2d(size.HalfWidth, size.HalfHeight);
-            this._grid.Draw(this._mapCache.getContext("2d"));
-            this._grid.Position = originalPosition;
+            this._mapCacheContext = this._mapCache.getContext("2d");
+            this._mapCacheContext.translate(size.HalfWidth, size.HalfHeight);
         }
 
-        private FillGridWith(mappings: number[][]): void {
-            var tiles: SquareTile[][] = [];
+        private CacheTile(tile: SquareTile): void {
+            // Draw the tile onto the map cache
+            tile.Draw(this._mapCacheContext);
+        }
 
-            for (var i = 0; i < mappings.length; i++) {
-                tiles[i] = [];
-                for (var j = 0; j < mappings[i].length; j++) {
-                    if (mappings[i][j] >= 0) {
-                        tiles[i].push(new SquareTile(this._Resources[mappings[i][j]], this._grid.TileSize.Width, this._grid.TileSize.Height));
-                    }
-                    else {
-                        tiles[i].push(null);
-                    }
+        private FillGridWith(mappings: number[][], onComplete: () => any): void {
+            asyncLoop((next: () => void, rowsComplete: number) => {
+                this.AsyncBuildGridRow(rowsComplete, mappings, () => {
+                    next();
+                });
+            }, mappings.length, () => {
+                onComplete();
+            });
+        }
+
+        private AsyncBuildGridTile(row: number, column: number, tileGraphic: Graphics.Assets.ImageSource, onComplete: (tile: SquareTile) => any): void {
+            var action = () => {
+                var tile: SquareTile;
+
+                tile = new SquareTile(tileGraphic, this._grid.TileSize.Width, this._grid.TileSize.Height);
+
+                this._grid.Fill(row, column, tile);
+
+                if (this._staticMap) {
+                    this.CacheTile(tile);
                 }
-            }
 
-            this._grid.FillSpace(0, 0, tiles);
+                this.OnTileLoad.Trigger(tile, this._tilesBuilt / this._totalTiles);
+
+                onComplete(tile);
+            };
+
+            if (this.TileLoadDelay.Milliseconds > 0) {
+                setTimeout(action, this.TileLoadDelay.Milliseconds);
+            }
+            else {
+                action();
+            }
+        }
+
+        // Only pretend async in order to free up the DOM
+        private AsyncBuildGridRow(rowIndex: number, mappings: number[][], onComplete: () => any): void {
+            var action = () => {
+                    asyncLoop((next: () => void , tilesLoaded: number) => {
+                        this._tilesBuilt++;
+
+                        if (mappings[rowIndex][tilesLoaded] >= 0) {
+                            this.AsyncBuildGridTile(rowIndex, tilesLoaded, this._Resources[mappings[rowIndex][tilesLoaded]], (tile: SquareTile) => {
+                                next();
+                            });
+                        }
+                        else {
+                            next();
+                        }
+                    }, mappings[rowIndex].length, () => {
+                        onComplete();
+                    });
+                };
+            
+            if (this.RowLoadDelay.Milliseconds > 0) {
+                setTimeout(action, this.RowLoadDelay.Milliseconds);
+            }
+            else {
+                action();
+            }
         }
     }
 
