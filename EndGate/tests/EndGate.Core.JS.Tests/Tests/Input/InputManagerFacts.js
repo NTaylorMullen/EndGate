8b96decf57cb0629a7b1aeb3683e6642b3413e93
N. Taylor Mullen <ntaylormullen@outlook.com>
2013-07-31 23:53:27 -0700

    Added InputManager tests to verify it's dispose functionality.
    #49

---
+++ EndGate/tests/EndGate.Core.JS.Tests/Tests/Input/InputManagerFacts.js
@@ -0,0 +1,26 @@
+﻿/// <reference path="../../Scripts/endgate.js" />
+
+(function () {
+
+    QUnit.module("InputManager Facts");
+
+    QUnit.test("InputManager dispose disposes its mouse and keyboard handler.", function () {
+        var inputManager = new eg.Input.InputManager(document.createElement("div"));
+
+        inputManager.Dispose();
+
+        QUnit.isTrue(inputManager.Mouse._disposed);
+        QUnit.isTrue(inputManager.Keyboard._disposed);
+    });
+
+    QUnit.test("InputManager dispose cannot be called twice.", function () {
+        var inputManager = new eg.Input.InputManager(document.createElement("div"));
+
+        inputManager.Dispose();
+
+        QUnit.throws(function () {
+            inputManager.Dispose();
+        });
+    });
+
+})();
