fc8957fd002c6ab7de73abc99b80ff45d1b77ac3
N. Taylor Mullen <ntaylormullen@outlook.com>
2013-08-01 00:14:29 -0700

    Added a tween test to verify that tweens dispose works as expected

--- EndGate/tests/EndGate.Core.JS.Tests/Tests/Tweening/TweenFacts.js
+++ EndGate/tests/EndGate.Core.JS.Tests/Tests/Tweening/TweenFacts.js
@@ -4,6 +4,26 @@
 
     QUnit.module("Tween Facts");
 
+    QUnit.test("Tween's dispose unbinds bound events and stops the tween.", function () {
+        var tween = new eg.Tweening.NumberTween(0, 3, eg.TimeSpan.FromSeconds(.3), eg.Tweening.Functions.Linear.EaseNone);
+
+        tween.OnChange.Bind(function () {});
+        tween.OnComplete.Bind(function () {});
+        
+        QUnit.isTrue(tween.OnChange.HasBindings());
+        QUnit.isTrue(tween.OnComplete.HasBindings());
+
+        tween.Play();
+
+        QUnit.isTrue(tween.IsPlaying());
+
+        tween.Dispose();
+
+        QUnit.isFalse(tween.OnChange.HasBindings());
+        QUnit.isFalse(tween.OnComplete.HasBindings());
+        QUnit.isFalse(tween.IsPlaying());
+    });
+
     QUnit.test("Tween's have onChange triggered on the last tween interval.", function () {
         var from = 0,
             to = 3,
