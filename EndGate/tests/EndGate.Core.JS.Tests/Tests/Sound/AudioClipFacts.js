ef863f69db986261829b151b4be623454dfe1306
N. Taylor Mullen <ntaylormullen@outlook.com>
2013-08-01 00:12:35 -0700

    Added tests to verify that AudioClip's dispose function works correctly.
    #51

---
+++ EndGate/tests/EndGate.Core.JS.Tests/Tests/Sound/AudioClipFacts.js
@@ -0,0 +1,29 @@
+﻿/// <reference path="../../Scripts/endgate.js" />
+
+(function () {
+
+    QUnit.module("Audio Clip Facts");
+
+    QUnit.test("AudioClip's dispose unbinds bound events.", function () {
+        var clip = new eg.Sound.AudioClip("");
+
+        clip.OnComplete.Bind(function () { });
+
+        QUnit.isTrue(clip.OnComplete.HasBindings());
+
+        clip.Dispose();
+
+        QUnit.isFalse(clip.OnComplete.HasBindings());
+    });
+
+    QUnit.test("AudioClip's throw if disposed more than once.", function () {
+        var clip = new eg.Sound.AudioClip("");
+
+        clip.Dispose();
+
+        QUnit.throws(function () {
+            clip.Dispose();
+        });
+    });
+
+})();
