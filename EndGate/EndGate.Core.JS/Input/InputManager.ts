b9d9417e5ead3452c4280e898671b87e011f10f0
N. Taylor Mullen <ntaylormullen@outlook.com>
2013-07-31 23:53:01 -0700

    Made the InputManager disposable.
    - Essentially just disposes the MouseHandler and KeyboardHandler.
    - Added the dispose event to the game's dispose.
    
    #49

--- EndGate/EndGate.Core.JS/Input/InputManager.ts
+++ EndGate/EndGate.Core.JS/Input/InputManager.ts
@@ -1,3 +1,4 @@
+/// <reference path="../Interfaces/IDisposable.ts" />
 /// <reference path="Mouse/MouseHandler.ts" />
 /// <reference path="Keyboard/KeyboardHandler.ts" />
 
@@ -6,7 +7,7 @@ module EndGate.Input {
     /**
     * Defines an all around Input handler which manages mouse and keyboard events.
     */
-    export class InputManager {
+    export class InputManager implements IDisposable {
         /**
         * Used to bind functions to mouse related events.
         */
@@ -16,13 +17,31 @@ module EndGate.Input {
         */
         public Keyboard: KeyboardHandler;
 
+        private _disposed: boolean;
+
         /**
         * Creates a new instance of the InputManager object.
         * @param target The object through which mouse events will be monitored on.
         */
         constructor(target: HTMLElement) {
+            this._disposed = false;
             this.Mouse = new MouseHandler(target);
             this.Keyboard = new KeyboardHandler();
+        }
+
+        /**
+        * Disposes the MouseHandler and unbinds all bound events.
+        */
+        public Dispose(): void {
+            if (!this._disposed) {
+                this._disposed = true;
+
+                this.Mouse.Dispose();
+                this.Keyboard.Dispose();
+            }
+            else {
+                throw new Error("MouseHandler cannot be disposed more than once");
+            }
         }
     }
 
