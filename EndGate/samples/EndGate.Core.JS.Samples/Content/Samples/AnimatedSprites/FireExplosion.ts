fc9776432dfdf1b91807bdaa6a2c0f880037c311
N. Taylor Mullen <ntaylormullen@outlook.com>
2013-08-06 12:13:49 -0700

    Updated the AnimatedSprites animation objects slightly

--- EndGate/samples/EndGate.Core.JS.Samples/Content/Samples/AnimatedSprites/FireExplosion.ts
+++ EndGate/samples/EndGate.Core.JS.Samples/Content/Samples/AnimatedSprites/FireExplosion.ts
@@ -8,9 +8,7 @@ module AnimatedSprites {
         public static BaseGraphic: eg.Graphics.ImageSource;
 
         constructor(x: number, y: number, onComplete: Function) {
-            var graphic = FireExplosion.BaseGraphic.Clone();
-
-            super(graphic, x, y, 128, 128, 18, 9, onComplete);
+            super(FireExplosion.BaseGraphic.Clone(), x, y, 128, 128, 18, 9, onComplete);
         }
     }
 
